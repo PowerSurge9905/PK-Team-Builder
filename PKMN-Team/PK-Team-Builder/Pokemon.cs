8d4657534de79040b4fd6ee813ca264d592e9da4
PowerSurge9905 <powersurge9905@gmail.com>
2025-11-03 15:58:26 -0500

    0.1.0
    Basic structure
    - Abstract Pokémon class
    - Moves, Natures, Species enums

---
+++ PKMN-Team/PK-Team-Builder/Pokemon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PK_Team_Builder
+{
+    internal abstract class Pokemon
+    {
+        protected Pokemon()
+        {
+        }
+
+        // Individual Values (IVs)
+        protected int hpIV;
+        protected int atkIV;
+        protected int defIV;
+        protected int spAtkIV;
+        protected int spDefIV;
+        protected int speedIV;
+
+        // Effort Values (EVs)
+        protected int hpEV;
+        protected int atkEV;
+        protected int defEV;
+        protected int spAtkEV;
+        protected int spDefEV;
+        protected int speedEV;
+
+        // Species and Base Stats
+        protected Species species;
+        protected int baseHP;
+        protected int baseAtk;
+        protected int baseDef;
+        protected int baseSpAtk;
+        protected int baseSpDef;
+        protected int baseSpeed;
+
+        protected Natures nature;
+
+        // TODO: Determine whether to declare moveset here or in each species' class
+    }
+}
