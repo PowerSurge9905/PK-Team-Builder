f92862e4b66650aae0a64720f34a81687fb407d8
PowerSurge9905 <powersurge9905@gmail.com>
2025-11-20 14:35:21 -0500

    0.1.10.4
    Added full Bulbasaur line as classes

---
+++ PKMN-Team/PK-Team-Builder/Individual Species/001 - 003 Bulbasaur Line/Ivysaur.cs
@@ -0,0 +1,28 @@
+﻿using PK_Team_Builder.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PK_Team_Builder.Individual_Species._001___003_Bulbasaur_Line
+{
+    internal class Ivysaur : Pokemon
+    {
+        public Ivysaur()
+        {
+            InitializePokemon(Species.Ivysaur);
+        }
+
+        public Ivysaur(int hpIV, int atkIV, int defIV, int spAtkIV, int spDefIV, int speedIV)
+        {
+            InitializePokemon(Species.Ivysaur);
+            this.hpIV = hpIV;
+            this.atkIV = atkIV;
+            this.defIV = defIV;
+            this.spAtkIV = spAtkIV;
+            this.spDefIV = spDefIV;
+            this.speedIV = speedIV;
+        }
+    }
+}
---
+++ PKMN-Team/PK-Team-Builder/Individual Species/001 - 003 Bulbasaur Line/Venusaur.cs
@@ -0,0 +1,29 @@
+﻿using PK_Team_Builder.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PK_Team_Builder.Individual_Species._001___003_Bulbasaur_Line
+{
+    internal class Venusaur : Pokemon
+    {
+        public Venusaur()
+        {
+            InitializePokemon(Species.Venusaur);
+        }
+
+        public Venusaur(int hpIV, int atkIV, int defIV, int spAtkIV, int spDefIV, int speedIV)
+        {
+            InitializePokemon(Species.Bulbasaur);
+            this.hpIV = hpIV;
+            this.atkIV = atkIV;
+            this.defIV = defIV;
+            this.spAtkIV = spAtkIV;
+            this.spDefIV = spDefIV;
+            this.speedIV = speedIV;
+        }
+    }
+}
